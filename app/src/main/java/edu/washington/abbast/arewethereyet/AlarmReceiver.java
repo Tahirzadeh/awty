85d23399112ba57db559c49206b11ac60482cf45
Abbas Tahirzadeh <Abbas@Abbass-MacBook-Air.local>
2017-02-22 15:03:57 -0800

    Finished Projects + EC + Screenshots

---
+++ app/src/main/java/edu/washington/abbast/arewethereyet/AlarmReceiver.java
@@ -0,0 +1,19 @@
+package edu.washington.abbast.arewethereyet;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+/**
+ * Created by Abbas on 2/22/17.
+ */
+
+public class AlarmReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        String message = "Texting " + intent.getStringExtra("number") + "\n\n " + intent.getStringExtra("message");
+        Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
+    }
+}
